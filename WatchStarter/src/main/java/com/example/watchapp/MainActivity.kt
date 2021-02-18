f0dcfb046d745b11658df112d408aabd99eefd86
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2021-02-18 16:11:30 -0700

    Small things

--- WatchStarter/src/main/java/com/example/watchapp/MainActivity.kt
+++ WatchStarter/src/main/java/com/example/watchapp/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.api.Verify
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
